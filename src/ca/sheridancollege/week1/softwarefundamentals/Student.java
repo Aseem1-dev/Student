2eb6b2aef19f6dc5f69527b9568c1223fb70ae5b
Aseem1-dev <65921667+Aseem1-dev@users.noreply.github.com>
2020-05-25 16:10:39 -0400

    Added today's date

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,7 +7,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 
 /**
  *
- * @author asidhu
+ * @author asidhu 25 May,2020
  */
 public class Student {
     
